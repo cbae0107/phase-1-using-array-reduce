0568f29b83ddeefaee0ee43584047348728ed717
Changhyun <chang.bae@flatironschool.com>
2023-03-06 19:15:31 -0800

    Using Array Reduce

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+const sum = totalBatteries
+const totalBatteries = batteryBatches.reduce((acc, battery) => acc + battery,0);
+console.log(sum)
